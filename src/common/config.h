7aca5eb4449b5d7b0ff2326fbab8b583ab666f5a
Tom Qi <>
2024-02-12 18:15:18 -0500

    Feature: General Bulk enq/deq for ACC100 Decoding

--- src/common/config.h
+++ src/common/config.h
@@ -248,6 +248,31 @@ class Config {
     return dir == Direction::kUplink ? this->ul_modulation_
                                      : this->dl_modulation_;
   }
+
+  inline size_t ModOrder(Direction dir) const {
+      if (dir == Direction::kUplink) {
+          if (ul_modulation_ == "16QAM") {
+              return 4;
+          } else if (ul_modulation_ == "QPSK") {
+              return 2;
+          } else if (ul_modulation_ == "64QAM") {
+              return 6;
+          } else {
+              return 0;
+          }
+      } else {
+          if (ul_modulation_ == "16QAM") {
+              return 4;
+          } else if (ul_modulation_ == "QPSK") {
+              return 2;
+          } else if (ul_modulation_ == "64QAM") {
+              return 6;
+          } else {
+              return 0;
+          }
+      }
+  }
+
   inline size_t ModOrderBits(Direction dir) const {
     return dir == Direction::kUplink ? this->ul_mod_order_bits_
                                      : this->dl_mod_order_bits_;
