a0ccc491c279f28984f270f20cae27c54de4cc52
cstandy <cst.andy@gmail.com>
2024-02-14 14:09:27 -0500

    Fix: Use large kFrameWnd to avoid jitter
    When the starting few frames exceed the deadline, the overall the performance
    may still meet the timing requirment. Some buffer will help us tolerate the
    jitter and measure the long-term behavior of the system. However, in the real
    world application, the buffer size can be small.

--- src/common/config.h
+++ src/common/config.h
@@ -250,27 +250,17 @@ class Config {
   }
 
   inline size_t ModOrder(Direction dir) const {
-      if (dir == Direction::kUplink) {
-          if (ul_modulation_ == "16QAM") {
-              return 4;
-          } else if (ul_modulation_ == "QPSK") {
-              return 2;
-          } else if (ul_modulation_ == "64QAM") {
-              return 6;
-          } else {
-              return 0;
-          }
-      } else {
-          if (ul_modulation_ == "16QAM") {
-              return 4;
-          } else if (ul_modulation_ == "QPSK") {
-              return 2;
-          } else if (ul_modulation_ == "64QAM") {
-              return 6;
-          } else {
-              return 0;
-          }
-      }
+    if (dir == Direction::kUplink) {
+      if (ul_modulation_ == "QPSK") { return 2; }
+      else if (ul_modulation_ == "16QAM") { return 4; }
+      else if (ul_modulation_ == "64QAM") { return 6; }
+      return 0;
+    } else {
+      if (dl_modulation_ == "QPSK") { return 2; }
+      else if (dl_modulation_ == "16QAM") { return 4; }
+      else if (dl_modulation_ == "64QAM") { return 6; }
+      return 0;
+    }
   }
 
   inline size_t ModOrderBits(Direction dir) const {
